3572e8a61d925a42b8ecb02185f25871d4baad03
PleahMaCaka <pleahmacaka@gmail.com>
2023-10-18 16:07:57 +0900

    feat: chat frame

---
+++ src/components/ChatFrame.svelte
@@ -0,0 +1,30 @@
+<script>
+    import ChatInput from "./ChatInput.svelte";
+    import ChatHistory from "./ChatHistory.svelte";
+</script>
+
+<div id="app">
+
+    <div id="chat-frame">
+        <ChatHistory/>
+        <ChatInput/>
+    </div>
+</div>
+
+<style>
+    #chat-frame {
+        border-color: #5272F2;
+        border-style: solid;
+        border-width: 4px;
+
+        box-shadow: 0 0 3px 2px #5272F2 inset;
+        border-radius: 25px;
+        width: 100%;
+        height: 90vh;
+    }
+
+    #app {
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+        border-radius: 25px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import ChatFrame from "../components/ChatFrame.svelte";
+</script>
+
+
+<div id="side-margin">
+    <ChatFrame/>
+</div>
+
+<style>
+    #side-margin {
+        margin-left: 20%;
+        margin-right: 20%;
+        padding-top: 5vh;
+    }
+</style>
